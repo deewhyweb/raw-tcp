07ff4119359c314d33540d11970b94d828296162
Philip Hayes <phayes@redhat.com>
2020-10-19 13:07:03 -0400

    Added tls support

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-var net = require('net');
-var server = net.createServer();    
-server.on('connection', handleConnection);
-server.listen(9000, function() {    
-  console.log('server listening to %j', server.address());  
-});
-function handleConnection(conn) {    
-  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
-  console.log('new client connection from %s', remoteAddress);
-  conn.on('data', onConnData);  
-  conn.once('close', onConnClose);  
-  conn.on('error', onConnError);
-  function onConnData(d) {  
-    console.log('connection data from %s: %j', remoteAddress, d);  
-    conn.write(d);  
-  }
-  function onConnClose() {  
-    console.log('connection from %s closed', remoteAddress);  
-  }
-  function onConnError(err) {  
-    console.log('Connection %s error: %s', remoteAddress, err.message);  
-  }  
-}
+#!/usr/bin/env node
+'use strict';
+
+const tls = require('tls');
+const fs = require('fs');
+const port = 9000;
+
+const options = {
+  key: fs.readFileSync('certs/server/server.key'),
+  cert: fs.readFileSync('certs/server/server.crt'),
+  ca: fs.readFileSync('certs/ca/ca.crt'), // authority chain for the clients
+  requestCert: true, // ask for a client cert
+  //rejectUnauthorized: false, // act on unauthorized clients at the app level
+};
+
+const server = tls.createServer(options, (socket) => {
+  socket.write('welcome!\n');
+  socket.setEncoding('utf8');
+  socket.pipe(socket);
+  socket.write('welcome!\n');
+})
+  .on('connection', () => {
+    console.log('insecure connection');
+  })
+
+  .on('secureConnection', (socket) => {
+    // c.authorized will be true if the client cert presented validates with our CA
+    console.log('secure connection; client authorized: ', socket.authorized);
+  })
+
+  .listen(port, () => {
+    console.log('server listening on port ' + port + '\n');
+  });
